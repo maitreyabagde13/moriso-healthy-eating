00f0c50c6268bd14fadc8ae6346bfd8db67917f8
Maitreya Bagde <102958203+maitreyabagde13@users.noreply.github.com>
2025-12-01 21:58:02 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig(({ mode }) => {
     plugins: [react()],
 
     // ⭐ IMPORTANT FOR GITHUB PAGES
-    base: "/moriso-healthy-eating/",
+    base: "./",
 
     define: {
       'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
